b1ed40e96a3cdba568614117d72d1747b0230585
Marcus Kreusch <marcus@kreusch-creative.de>
2012-04-11 12:11:49 +0200

    update

--- xtcm_db.class.php
+++ xtcm_db.class.php
@@ -28,11 +28,20 @@ class xtcm_db{
     }
     
     public static function get_product_value($pid, $col){
+          
+           $q = "SELECT $col FROM products WHERE products_id = $pid";
+           $rs = xtc_db_query($q);
+           $r = mysql_fetch_object($rs);
+           return $r->{$col};
+    
+    }
+    
+    public static function get_products_description_value($pid, $col, $lid = ''){
            
            if($lid == ''){
               $lid = $_SESSION["languages_id"];
            }
-           $q = "SELECT $col FROM products WHERE products_id = $pid";
+           $q = "SELECT $col FROM products_description WHERE products_id = $pid AND language_id = $lid";
            $rs = xtc_db_query($q);
            $r = mysql_fetch_object($rs);
            return $r->{$col};
